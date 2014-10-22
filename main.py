ab50a9b075e15ab41465e682ac1876f9600408fc
Eamonn de Leastar <edeleastar@gmail.com>
2014-10-22 07:16:04 +0100

    introduce manager

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from app import createApp
+from flask.ext.script import Manager
 
 app = createApp()
+manager = Manager(app)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  #app.run(debug=True)
+  manager.run()
