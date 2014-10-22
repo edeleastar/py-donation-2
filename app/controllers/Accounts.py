33b0bbe8e08ae7ba35514a99e7bedd7897d8d302
Eamonn de Leastar <edeleastar@gmail.com>
2014-10-22 06:51:16 +0100

    full accounts blueprint implemented

--- app/controllers/Accounts.py
+++ app/controllers/Accounts.py
@@ -1,7 +1,40 @@
-from flask import render_template
+from flask import render_template, session, request, redirect
 from . import accounts
+from app.models.User import User
+from app.models.User import users
 
 
 @accounts.route('/')
 def index():
   return render_template('accounts/index.html')
+
+@accounts.route('/login')
+def login():
+  return render_template('accounts/login.html')
+
+@accounts.route('/signup')
+def logout():
+  return render_template('accounts/signup.html')
+
+@accounts.route('/logout')
+def signup():
+  return render_template('accounts/index.html')
+
+@accounts.route('/register', methods=['POST'])
+def register():
+  firstname = request.form['firstName']
+  lastname  = request.form['lastName']
+  email     = request.form['email']
+  password  = request.form['password']
+  users[email] = (User (firstname, lastname, email, password))
+  return redirect('/')
+
+@accounts.route('/authenticate', methods=['POST'])
+def authenticate():
+  email     = request.form['email']
+  password  = request.form['password']
+  if email in users and password == users[email].password:
+    session['logged_in'] = True
+    return redirect('/donation')
+  else:
+    return redirect('/')
--- app/models/User.py
+++ app/models/User.py
@@ -1,8 +1,31 @@
-
+users = {}
+userId = 1
 
 class User:
   def __init__(self, firstname, lastname, email, password):
+    global userId
+
     self.firstname = firstname
     self.lastname = lastname
     self.email = email
     self.password = password
+    self.id = userId
+    userId += 1
+
+  def toJson(self):
+    json_user = \
+    {
+      'firstname' : self.firstname,
+      'lastname'  : self.lastname,
+      'email'     : self.email,
+      'password'  : self.password
+    }
+    return json_user
+
+  @staticmethod
+  def findById(id):
+    global users
+    for user in users.values():
+      if user.id == id:
+        return user
+    return None
