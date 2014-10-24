2401ec822bf59ebd33d391fb79c942422422bb6c
Eamonn de Leastar <edeleastar@gmail.com>
2014-10-24 12:47:12 +0100

    introduce wtfforms support to simplify accounts controller

--- app/controllers/Accounts.py
+++ app/controllers/Accounts.py
@@ -1,37 +1,48 @@
 from flask import render_template, session, request, redirect
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from . import accounts
 from app.models.User import User
+
+
+class LoginForm(Form):
+  email    = StringField('Email',    validators=[DataRequired()])
+  password = StringField('Password', validators=[DataRequired()])
+
+class SignupForm(Form):
+  firstname = StringField('First Name', validators=[DataRequired()])
+  lastname  = StringField('last Name',  validators=[DataRequired()])
+  email     = StringField('Email',      validators=[DataRequired()])
+  password  = StringField('Password',   validators=[DataRequired()])
 
 @accounts.route('/')
 def index():
   return render_template('accounts/index.html')
 
-@accounts.route('/login')
+@accounts.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('accounts/login.html')
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.objects(email=form.email.data).first()
+    if user != None:
+      if user.email == form.password.data:
+        session['logged_in'] = True
+        return redirect('/donation')
+  return render_template('accounts/login.html', form=form)
 
-@accounts.route('/signup')
+@accounts.route('/signup', methods=['GET', 'POST'])
 def logout():
-  return render_template('accounts/signup.html')
+  form = SignupForm()
+  if form.validate_on_submit():
+    user = User()
+    form.populate_obj(user)
+    user.ID=User.objects.count() + 1
+    user.save()
+    return redirect('/login')
+  return render_template('accounts/signup.html', form=form)
 
 @accounts.route('/logout')
 def signup():
+  session['logged_in'] = None
   return render_template('accounts/index.html')
-
-@accounts.route('/register', methods=['POST'])
-def register():
-  user = User (firstname=request.form['firstName'], lastname=request.form['lastName'],
-               email=request.form['email'], password  = request.form['password'],
-               ID=User.objects.count() + 1)
-  user.save()
-  return redirect('/')
-
-@accounts.route('/authenticate', methods=['POST'])
-def authenticate():
-  matches = User.objects(email=request.form['email'])
-  if matches.count() > 0:
-    user = matches.get(0)
-    if user.email == request.form['password']:
-      session['logged_in'] = True
-      return redirect('/donation')
-  return redirect('/')
